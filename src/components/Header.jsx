90313915d1992630f15febb566d1b40ef530ed52
fikripcr <fikri@pcr.ac.id>
2026-06-05 14:51:17 +0700

    ubah hello menjadi hai

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
                 {/* Profile Section */}
                 <div id="profile-container" className="flex items-center space-x-4 border-l pl-4 border-gray-300">
                     <span id="profile-text">
-                        Hello, <b>Fikri Muhaffizh</b>
+                        Hai, <b>Fikri Muhaffizh</b>
                     </span>
                     <img
                         id="profile-avatar"
