f9664cc82f90569b61aa152024d75f55b82200d4
fikripcr <fikri@pcr.ac.id>
2026-06-05 14:48:59 +0700

    hapus icon chart dan settings di header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,13 +24,7 @@ export default function Header() {
                     <FaBell />
                     <span id="notification-badge" className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-blue-200 rounded-full px-2 py-1 text-xs">50</span>
                 </div>
-                <div id="chart-icon" className="p-3 bg-blue-100 rounded-2xl cursor-pointer">
-                    <FcAreaChart />
-                </div>
-                <div id="settings-icon" className="p-3 bg-red-100 rounded-2xl text-red-500 cursor-pointer">
-                    <SlSettings />
-                </div>
-             
+
 
                 {/* Profile Section */}
                 <div id="profile-container" className="flex items-center space-x-4 border-l pl-4 border-gray-300">
@@ -39,7 +33,7 @@ export default function Header() {
                     </span>
                     <img
                         id="profile-avatar"
-                        src="https://avatar.iran.liara.run/public/28"
+                        src="https://avatar-placeholder.iran.liara.run/public/28"
                         className="w-10 h-10 rounded-full"
                     />
                 </div>
